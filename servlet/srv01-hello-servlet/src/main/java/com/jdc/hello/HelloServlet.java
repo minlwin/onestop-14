269ccdc2165dbf152ef0c6a209059ffbc1c894db
Min Lwin <lwin.zawmin@gmail.com>
2025-09-10 16:07:33 +0630

    Update Hello Servlet

---
+++ servlet/srv01-hello-servlet/src/main/java/com/jdc/hello/HelloServlet.java
@@ -0,0 +1,32 @@
+package com.jdc.hello;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().append("""
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<title>My First Servlet</title>
+			</head>
+			<body>
+				<h1>Hello Servlet</h1>
+			</body>
+			</html>
+			""");
+	}
+}
